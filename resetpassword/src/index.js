0d9a0b058b9ffaae00fd509c7eb7105481cbf6e9
Narasimman2067 <narasimmamech2067@gmail.com>
2023-05-16 13:37:12 +0530

    reset

---
+++ resetpassword/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import { BrowserRouter } from "react-router-dom"
+import Context from './Components/ContextProvider/Context';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Context>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Context>
+
+);
+
+reportWebVitals();
